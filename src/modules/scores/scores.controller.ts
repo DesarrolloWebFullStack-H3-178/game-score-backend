4179e406c52d35ced6e5ade13ecf11934339c294
Camilo Quiroga <camilo.quirogam@gmail.com>
2024-11-15 13:51:03 -0500

    Feature(Backend):Swagger into Application definition

--- src/modules/scores/scores.controller.ts
+++ src/modules/scores/scores.controller.ts
@@ -4,7 +4,7 @@ import { Scores } from './scores.schema';
 import { UpdateScoresDto } from './dto/update-scores.dto';
 import { CreateScoresDto } from './dto/create-scores.dto';
 import { PaginationQueryDto } from 'src/commons/dto/pagination-query.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {ApiTags, ApiOperation, ApiResponse} from '@nestjs/swagger';
 
 @ApiTags('Scores')
 @Controller('scores')
@@ -15,27 +15,36 @@ export class ScoresController {
 
   @Get('admin')
   @ApiOperation({summary: 'Get the list of All Scores as Admin'})
+  @ApiResponse({status: 201, description: 'List users with highest scores', type: Scores} )
   async getAllScoresAdmin(@Query() paginationQuery: PaginationQueryDto): Promise<{ data: Scores[], total: number, page: number, limit: number, totalPages: number }> {
     return this.scoresService.getAllScores(paginationQuery.limit, paginationQuery.page);
   }
 
   @Get('leaderboard')
+  @ApiOperation({summary: 'Get the list of best players'})
+  @ApiResponse({status: 200, description: 'List users with highest scores', type: Scores} )
   async leaderBoard(@Query('limit') limit: string): Promise<Scores[]> {
     const topScoresLimit = parseInt(limit, 10) || 10;
     return this.scoresService.leaderBoard(topScoresLimit);
   }
 
   @Put(':scoreId')
+  @ApiOperation({summary: 'Update data for one score'})
+  @ApiResponse({status: 200, description: 'Score Blocked / Unblocked', type: Scores} )
   async updateScore(@Param('scoreId') id: string, @Body() updateScoreDto: UpdateScoresDto): Promise<Scores> {
     return this.scoresService.updateScore(id, updateScoreDto);
   }
 
   @Get(':scoreId')
+  @ApiOperation({summary: 'get Score by Id'})
+  @ApiResponse({status: 200, description: 'Get One Score', type: Scores} )
   async getScoreById(@Param('scoreId') scoreId: string): Promise<Scores> {
     return this.scoresService.getScoreById(scoreId);
   }
 
   @Post()
+  @ApiOperation({summary: 'Create a score to an User'})
+  @ApiResponse({status: 201, description: 'Score created successfully', type: Scores} )
   async createScore(@Body() createScoreDto: CreateScoresDto): Promise<Scores> {
     return this.scoresService.createScore(createScoreDto);
   }
@@ -43,6 +52,8 @@ export class ScoresController {
   // ======= Admin ===========
 
   @Patch('admin/:scoreId')
+  @ApiOperation({summary: 'Block or Unblock an Score as Admin'})
+  @ApiResponse({status: 200, description: 'Score Blocked / Unblocked', type: Scores} )
   async blockScoreByIdAdmin(@Param('scoreId') id: string): Promise<Scores> {
     const score = await this.scoresService.scoreStatus(id);
     if (!score) {
@@ -52,11 +63,15 @@ export class ScoresController {
   }
 
   @Put('admin/:id')
+  @ApiOperation({summary: 'Update data for one score as Admin permissions'})
+  @ApiResponse({status: 200, description: 'Score Blocked / Unblocked', type: Scores} )
   async adminUpdateScore(@Param('id') id: string, @Body() updateScoreDto: UpdateScoresDto): Promise<Scores> {
     return this.scoresService.adminUpdateScore(id, updateScoreDto);
   }
 
   @Get('admin/:id')
+  @ApiOperation({summary: 'Block or Unblock an Score as Admin'})
+  @ApiResponse({status: 200, description: 'Score Blocked / Unblocked', type: Scores} )
   async getAdminScoreById(@Param('id') id: string): Promise<Scores> {
     return this.scoresService.getAdminScoreById(id);
   }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,6 +8,8 @@ import { PaginationQueryDto } from 'src/commons/dto/pagination-query.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
+import {ApiTags, ApiOperation, ApiResponse} from '@nestjs/swagger';
+
 
 @Controller('users')
 export class UsersController {
@@ -16,26 +18,36 @@ export class UsersController {
     // ============== Auth ================
 
     @Post('auth/register')
+    @ApiOperation({summary: 'Create User (Register)'})
+    @ApiResponse({status: 201, description: 'User created successfully', type: UsersService} )
         userRegister(@Body() createUserDto: CreateUserDto): User {
         return this.usersService.createUser(createUserDto);
     }   
     @Post('auth/login')
+    @ApiOperation({summary: 'Login user (Auth)'})
+    @ApiResponse({status: 201, description: 'User Login successfull', type: UsersService} )
         userLogin(@Body() loginUserDto: LoginUserDto): LoginResponse {
         return this.usersService.loginUser(loginUserDto);
     }
 
     // ========== User ===========
     @Get('profile/:id')
+    @ApiOperation({summary: 'Get detailed data of user'})
+    @ApiResponse({status: 200, description: 'User data', type: UsersService} )
         getUserById(@Param('id') id: string ): User | undefined {
         return this.usersService.getProfileUserById(id);
     }
 
     @Put('profile/:id')
+    @ApiOperation({summary: 'Update user'})
+    @ApiResponse({status: 200, description: 'User information successfully modified', type: UsersService} )
         userUpdate(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): User {
         return this.usersService.updateUser(id, updateUserDto);
     }
 
     @Get('session/:id')
+    @ApiOperation({summary: 'Session of User status'})
+    @ApiResponse({status: 200, description: 'Session of User status', type: UsersService} )
         getUserSessionId(@Param('id') id: string): SessionResponse {
         return this.usersService.getSessionUserById(id);
     }
@@ -43,11 +55,15 @@ export class UsersController {
     // ========== Admin Users ==========
 
     @Put('admin/profile/:id')
+    @ApiOperation({summary: 'Update user as Admin'})
+    @ApiResponse({status: 200, description: 'user information successfully modified', type: UsersService} )
         userAdminUpdate(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): User {
         return this.usersService.adminUpdateUser(id, updateUserDto);
     }
 
     @Patch('admin/:id')
+    @ApiOperation({summary: 'Block or Unblock an User as Admin'})
+    @ApiResponse({status: 200, description: 'User Blocked / Unblocked', type: UsersService} )
         blockUserAdmin(@Param('id') id: string): User {
         const user = this.usersService.userStatus(id);
         if (!user) {
@@ -57,6 +73,8 @@ export class UsersController {
     }
     
     @Get('admin')
+    @ApiOperation({summary: 'Get detailed data of user as Admin'})
+    @ApiResponse({status: 200, description: 'Users data', type: UsersService} )
     getAllUsersAdmin(@Query() paginationQuery: PaginationQueryDto) {
         return this.usersService.getAllUsers(paginationQuery);
     }
@@ -64,6 +82,8 @@ export class UsersController {
     /* @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('Admin') */
     @Get('admin/:id')
+    @ApiOperation({summary: 'Get detailed data of user as Admin'})
+    @ApiResponse({status: 200, description: 'User data', type: UsersService} )
         getUserByIdAdmin(@Param('id') id: string ): User | undefined {
         return this.usersService.getAdminUserById(id);
     }
